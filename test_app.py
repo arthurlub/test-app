6c4eae99ac3718258cdee90eae870ae73a04256d
Arthur <arthur931@gmail.com>
2025-09-01 17:57:59 +0300

    test 2

--- test_app.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app import app
-
-def test_hello():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.data == b'Hello, World!'
-        assert response.status_code == 200
